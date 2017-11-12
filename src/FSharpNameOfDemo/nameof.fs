af2e72777c648ec87d1d7365b968576869df49f8
boloutaredoubeni <tarecd@gmail.com>
2017-11-11 21:42:49 -0500

    Fix type errors

--- src/FSharpNameOfDemo/nameof.fs
+++ src/FSharpNameOfDemo/nameof.fs
@@ -2,6 +2,9 @@ namespace FSharpNameOfDemo
 
 open Microsoft.FSharp.Compiler.SourceCodeServices
 open Microsoft.FSharp.Compiler.Ast
+open Nameof
+open Nameof
+open Nameof
 
 type AsyncResult<'TSuccess, 'TFailure> = Async<Result<'TSuccess, 'TFailure>>
 
@@ -12,27 +15,37 @@ module Nameof =
 
   let [<Literal>] NAMEOF = "nameof"
 
-  type Ident with member ident.IsNameOfOperator () = ident.idText = NAMEOF
+  type Ident with 
+    member ident.IsNameOfOperator () = ident.idText = NAMEOF
+    member ident.NameOf () = 
+      let name = ident.idText
+      let range = ident.idRange
+      let stringConst = SynConst.String (name, range)
+      SynExpr.Const (stringConst, range)
 
-  type SynExpr with 
-    member expr.NameOf () = 
-      match expr with
-      | SynExpr.Ident ident -> Some ident.idText
-      | SynExpr.LongIdent (_, [ident], _, _) | SynExpr.LongIdent (_, _::[ident], _, _) -> Some ident.idText
-      | _ -> None
+  type NameOfError =  FailedToParseLongId | NotAnIdentifier
 
-  /// 'Fast is F# AST
-  type Namer<'Fast when 'Fast: (member NameOf: unit -> option<string>)> = 'Fast
-  // Only accounts for usage in Expressions, not Modules, types or namespaces, attributes, patterns etc
-  let inline nameof (ident: Namer<_>): option<string> = ident.NameOf()
-  let mapNameof = function
+  let nameof = function
+    | SynExpr.Ident ident -> Result.Ok (ident.NameOf())
+    | SynExpr.LongIdent (_, idents, _, _) -> 
+      let lid = idents.Lid
+      match lid with
+      | [ident] | _::[ident] -> Result.Ok (ident.NameOf())
+      | _ -> Error FailedToParseLongId
+    | _ -> Error NotAnIdentifier
+
+  
+  /// Only accounts for usage in Expressions, not Modules, types or namespaces, attributes, patterns etc
+  let mapNameof synExpr =
+    match synExpr with
     | SynExpr.App (_, false, functionExpression, argumentExpression, _) -> 
       match functionExpression with
       | SynExpr.Ident ident when ident.IsNameOfOperator() -> 
-        // FIXME: construct the new tree
-        nameof argumentExpression
-      | _ -> None
-    | _ -> None 
+        do nameof argumentExpression |> ignore
+        // FIXME: Do inplace refactor
+        synExpr
+      | _ -> synExpr
+    | _ -> synExpr
   
   let private getProjectOptionsFromScript file input =
     async {
